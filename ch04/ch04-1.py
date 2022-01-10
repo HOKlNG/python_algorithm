713adaeaad2542d82419385e0ebe850900762acd
HOKlNG <ghtjd22103@gmail.com>
2022-01-10 22:19:09 +0900

    'ch05'

--- ch04/ch04-1.py
+++ ch04/ch04-1.py
@@ -1,23 +1,31 @@
-import time
+## 4-1 상하좌우
+## 풀이 일자 2022-01-10
+## 시간 13/15 소요
+## 정답과는 다소 상이
+## 답안 체크 여부 N
 
-n = int(input())
-move_list =  input().split()
+n = int(input().split()[0])
 
-st = time.time()
-x, y = 1, 1
+plan = input().split()
 
-for m in move_list:
-    if m == 'L' and y > 1:
-        y -= 1
-    elif m == 'R' and y < n :
-        y += 1
-    elif m == 'U' and x > 1 :
+print(n)
+
+print(plan)
+
+x = 1 #가로
+y = 1 #세로
+# (y,x)
+
+for move in plan:
+    print(move)
+    print(y, x)
+    if move == 'L' and x > 1:
         x -= 1
-    elif m == 'D' and x < n :
-        x += 1
-    else:
-        #out area
-        pass
-        print(m)
+    elif move == 'R' and x < n:
+        x +=1
+    elif move == 'U' and y > 1:
+        y -=1
+    elif move == 'D' and y < n:
+        y +=1
 
-print(x,y)
+print(str(y)+' '+str(x))
